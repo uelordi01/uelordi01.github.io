b90897580ccdc9e0f3122d45259d44f5372c3687
VICOMTECH\uelordi <uelordi@vicomtech.org>
2024-03-20 10:36:57 +0100

    adding new content to the repo

---
+++ lang.js
@@ -0,0 +1,22 @@
+var translations = {
+    'en': {
+      'title': 'Unai Elordi',
+      'menu_home': 'Home',
+      'menu_resume': 'Resume',
+      'h1_headline':'<b>15</b> years applying <b>AI & CV research</b> to solve real-world problems',
+      'h2_headline':'<b>AI Speaker</b> featured in',
+      'featured': '<span class="typed" data-typed-items="Conferences, Eitb, Naiz Irratia, "></span>',
+      // Add more translations
+    },
+    'eus': {
+      'title': 'Unai Elordi P',
+      'menu_home': 'Hasiera',
+      'menu_resume': 'kurrikuluma',
+      'h1_headline':'<b>15</b> urte mundu errealeko arazoak <b>Adimen Artifizialaren</b> bitartez ikertzen eta aplikatzen',
+      'h2_headline':'<b>Hizlari</b> eta <b>kolaboratzaile:</b>',
+      'featured':'<span class="typed" data-typed-items="Konferentzietan, Eitb-n, Naiz Irratian"></span>'
+      // Add more translations
+    },
+    // Add more languages
+  };
+  
